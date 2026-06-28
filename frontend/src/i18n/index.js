af90fbaba8ebfbaec2e1eec1b26c903888e59e77
Roman Matviy <roman@matviy.pp.ua>
2026-06-28 21:05:09 +0300

    feat: add i18next localization configuration and application shell template

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -2,11 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import translations from "./translations.js";
 
+const resources = {};
+Object.keys(translations).forEach(lng => {
+  resources[lng] = { translation: translations[lng] };
+});
+
 i18n.use(initReactI18next).init({
   lng: localStorage.getItem("lang") || "uk",
   fallbackLng: "uk",
   interpolation: { escapeValue: false },
-  resources: translations,
+  ns: ["translation"],
+  defaultNS: "translation",
+  resources,
 });
 
 export default i18n;
--- public_html/app/assets/Sites-C-Z3LoM5.js
+++ public_html/app/assets/Sites-C-Z3LoM5.js
@@ -1,4 +1,4 @@
-import{j as e}from"./vendor-query-BEUqgBPG.js";import{r as x}from"./vendor-router-nbMWPCiB.js";import{S as W,E as C}from"./EditSiteModal-B3dtj0Lh.js";import{u as R,a as I,B as v}from"./App-BlVbw-6f.js";import{C as r}from"./index-Cxm5u9Pe.js";import"./client-DbjFF3zR.js";import"./useTranslation-pA58JZ2t.js";const $=`
+import{j as e}from"./vendor-query-BEUqgBPG.js";import{r as x}from"./vendor-router-nbMWPCiB.js";import{S as W,E as C}from"./EditSiteModal-IlAJEy_M.js";import{u as R,a as I,B as v}from"./App-D0H1lanm.js";import{C as r}from"./index-XCYN986x.js";import"./client-DbjFF3zR.js";import"./useTranslation-DLpxcZMk.js";const $=`
   @media (max-width: 640px) {
     .sites-limit-banner { flex-direction: column !important; align-items: flex-start !important; gap: 10px !important; }
     .sites-limit-banner .sites-limit-btn { width: 100%; }
