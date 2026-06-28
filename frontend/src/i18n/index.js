e514b9a4db3246ff41bd6df6d93c8642bcc4411e
Roman Matviy <roman@matviy.pp.ua>
2026-06-28 20:41:09 +0300

    feat: implement billing and subscription management UI with i18n support

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -1,6 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+throw new Error("I18N INITIALIZATION LOADED - THIS SHOULD CRASH THE APP");
+
 i18n.use(initReactI18next).init({
   lng: "uk",
   fallbackLng: "uk",
