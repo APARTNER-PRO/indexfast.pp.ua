791a62cbd92860ff54f1ae2c35e19de4291a208d
Roman Matviy <roman@matviy.pp.ua>
2026-06-28 21:57:20 +0300

    feat: add base build configuration and initial application entry point

--- public_html/app/assets/Overview-BkPJZ3ze.js
+++ public_html/app/assets/Overview-BkPJZ3ze.js
@@ -1,4 +1,4 @@
-import{r as w,j as e,C as t}from"./index-CdsEkt1q.js";import{u as L,B as h,S as _,a as I,P as F}from"./App-DLLRDRUg.js";import{S as P,E as M}from"./EditSiteModal-C1nUD3dj.js";import{u as O}from"./useTranslation-CmiZBHOV.js";import"./client-DbjFF3zR.js";const D=`
+import{r as w,j as e,C as t}from"./index-DlWH7H6P.js";import{u as L,B as h,S as _,a as I,P as F}from"./App-DVvbCEsg.js";import{S as P,E as M}from"./EditSiteModal-BKYofiTs.js";import{u as O}from"./useTranslation-Dc56Qlr6.js";import"./client-DbjFF3zR.js";const D=`
   @media (max-width: 640px) {
     .ov-upgrade { flex-direction: column !important; align-items: flex-start !important; padding: 16px !important; gap: 12px !important; }
     .ov-upgrade .ov-upgrade-btn { width: 100%; }
