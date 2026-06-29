6bd2b28f03fe2a2b12061638e8c7813f33531c9e
Roman Matviy <roman@matviy.pp.ua>
2026-06-29 13:19:13 +0300

    feat: add main dashboard shell, i18n translation structure, and edit site modal asset

--- public_html/app/assets/Overview-gZp81X9R.js
+++ public_html/app/assets/Overview-gZp81X9R.js
@@ -1,4 +1,4 @@
-import{r as m,i as f,j as e,C as t}from"./index-DNSr56nY.js";import{u as I,B as y,S as F,a as P,P as M}from"./App-nXL1kvKw.js";import{S as O,E as D}from"./EditSiteModal-C25RiS3c.js";import"./client-DbjFF3zR.js";const U=`
+import{r as m,i as f,j as e,C as t}from"./index-CvhsMlXv.js";import{u as I,B as y,S as F,a as P,P as M}from"./App-C5Gw17TO.js";import{S as O,E as D}from"./EditSiteModal-DCi7xDXb.js";import"./client-DbjFF3zR.js";const U=`
   @media (max-width: 640px) {
     .ov-upgrade { flex-direction: column !important; align-items: flex-start !important; padding: 16px !important; gap: 12px !important; }
     .ov-upgrade .ov-upgrade-btn { width: 100%; }
