c0ed1d9811aecaae0cf428ce22406d2c1e12e29d
Roman Matviy <roman@matviy.pp.ua>
2026-06-30 00:00:36 +0300

    feat: add internationalization translation module and initialize frontend application shell

--- public_html/app/assets/EditSiteModal-Ddl05wBJ.js
+++ public_html/app/assets/EditSiteModal-Ddl05wBJ.js
@@ -1,4 +1,4 @@
-import{r as c,j as e,C as s,i as M,w as Q}from"./index-dFkH0GnB.js";import{g as A,P as J,h as P,B as h,M as V,F as W,I as O,T as X,K as Z}from"./App-DvuNq7qB.js";import{a as ee}from"./client-DbjFF3zR.js";const te=`
+import{r as c,j as e,C as s,i as M,w as Q}from"./index-DzHOYRCb.js";import{g as A,P as J,h as P,B as h,M as V,F as W,I as O,T as X,K as Z}from"./App-BrL8krD2.js";import{a as ee}from"./client-DbjFF3zR.js";const te=`
   @media (max-width: 640px) {
     .sites-table-wrap table { display: none; }
     .sites-mobile-list { display: flex !important; }
