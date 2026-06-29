faa8f13f5741d4459a9ab79ab8dc909ba2136816
Roman Matviy <roman@matviy.pp.ua>
2026-06-29 19:21:47 +0300

    feat: add EditSiteModal component and supporting i18n translations with initial app entry page

--- public_html/app/assets/Sites-BxLWzGkX.js
+++ public_html/app/assets/Sites-BxLWzGkX.js
@@ -1,4 +1,4 @@
-import{r as d,i as c,j as e,C as s}from"./index-B9YHCweD.js";import{S as R,E as I}from"./EditSiteModal-DhoskVDQ.js";import{u as $,a as A,B as S}from"./App-B5gJ75lQ.js";import"./client-DbjFF3zR.js";const M=`
+import{r as d,i as c,j as e,C as s}from"./index-DTDwR4n1.js";import{S as R,E as I}from"./EditSiteModal-jsqh65J0.js";import{u as $,a as A,B as S}from"./App-DXUZ_TIC.js";import"./client-DbjFF3zR.js";const M=`
   @media (max-width: 640px) {
     .sites-limit-banner { flex-direction: column !important; align-items: flex-start !important; gap: 10px !important; }
     .sites-limit-banner .sites-limit-btn { width: 100%; }
