4e8565173a1d5f043efb736c70ec668f34230bcb
Roman Matviy <roman@matviy.pp.ua>
2026-06-29 18:31:51 +0300

    feat: add GscMetrics page for Google Search Console data visualization

--- public_html/app/assets/EditSiteModal-PZW9DdqR.js
+++ public_html/app/assets/EditSiteModal-PZW9DdqR.js
@@ -1,4 +1,4 @@
-import{r as d,j as e,C as i,w as K}from"./index-DmqO_Efj.js";import{g as W,P as J,h as G,B as h,M as q,F as N,I as $,T as Q,K as V}from"./App-DQcKgJQZ.js";import{a as X}from"./client-DbjFF3zR.js";const Z=`
+import{r as d,j as e,C as i,w as K}from"./index-WDiulc4k.js";import{g as W,P as J,h as G,B as h,M as q,F as N,I as $,T as Q,K as V}from"./App-BQNvEKxs.js";import{a as X}from"./client-DbjFF3zR.js";const Z=`
   @media (max-width: 640px) {
     .sites-table-wrap table { display: none; }
     .sites-mobile-list { display: flex !important; }
