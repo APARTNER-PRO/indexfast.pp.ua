5fb37b957df34d553080169271679efcd3e233e4
Roman Matviy <roman@matviy.pp.ua>
2026-06-30 12:08:15 +0300

    feat: implement multi-language i18n support and system translation utilities

--- public_html/app/assets/Overview-DB8B6yZv.js
+++ public_html/app/assets/Overview-DB8B6yZv.js
@@ -1,4 +1,4 @@
-import{r as m,i as g,j as e,C as t}from"./index-j8ZMRBa8.js";import{u as I,B as y,S as F,a as P,P as M}from"./App-BdajN-2-.js";import{S as O,E as D}from"./EditSiteModal-Dm1tC909.js";import"./client-DbjFF3zR.js";const U=`
+import{r as m,i as g,j as e,C as t}from"./index-CCLd9qKh.js";import{u as I,B as y,S as F,a as P,P as M}from"./App-Db_ppmBn.js";import{S as O,E as D}from"./EditSiteModal-ncIGrLbA.js";import"./client-DbjFF3zR.js";const U=`
   @media (max-width: 640px) {
     .ov-upgrade { flex-direction: column !important; align-items: flex-start !important; padding: 16px !important; gap: 12px !important; }
     .ov-upgrade .ov-upgrade-btn { width: 100%; }
